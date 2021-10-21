92c2c08e8717fa5ab7ada7b59ddef58b1dd12d09
bendesmedt <benjamin.desmedt@student.intecbrussel.be>
2021-10-21 09:14:35 +0200

    pervy button

--- src/main/java/be/bacdesmedt/javafxdemo/ButtonClicker.java
+++ src/main/java/be/bacdesmedt/javafxdemo/ButtonClicker.java
@@ -2,20 +2,19 @@ package be.bacdesmedt.javafxdemo;
 
 import javafx.application.Application;
 import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
-import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.ButtonType;
 import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
+import javafx.stage.WindowEvent;
 
-import java.io.IOException;
+import java.util.Optional;
 
 public class ButtonClicker extends Application{
 
-    public static final String TITLE = "JavaFX Demo";
+    public static final String TITLE = "Pervy Button";
 
     Button button;
 
@@ -28,18 +27,11 @@ public class ButtonClicker extends Application{
     public void start(Stage stage) {
 
         stage.setTitle(TITLE);
-        /*FXMLLoader fxmlLoader = new FXMLLoader(MainApplication.class.getResource("main-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setScene(scene);*/
-
         button = new Button();
-        button.setText("click me");
-        button.setOnAction(e -> {
-            new Alert(Alert.AlertType.NONE,
-                    "I love it when you touch me like that",
-                    ButtonType.CLOSE
-            ).show();
-        });
+        button.setText("Touch me");
+        button.setOnAction(this::showAlert);
+
+        stage.setOnCloseRequest(this::onClose);
 
         StackPane layout = new StackPane();
         layout.getChildren().add(button);
@@ -49,4 +41,25 @@ public class ButtonClicker extends Application{
 
         stage.show();
     }
+
+    private void showAlert(ActionEvent e) {
+        new Alert(Alert.AlertType.NONE,
+                "I love it when you touch me like that",
+                ButtonType.CLOSE
+        ).show();
+    }
+
+    private void onClose(WindowEvent e) {
+        Alert alert = new Alert(Alert.AlertType.CONFIRMATION);
+        alert.setTitle("You are ending our time together");
+        alert.setHeaderText("Are you sure?");
+        alert.setContentText("I haven't touched you yet!");
+
+        Optional<ButtonType> result = alert.showAndWait();
+        if (result.get() == ButtonType.CANCEL){
+            e.consume();
+        }
+    }
+
 }
+
--- src/main/java/be/bacdesmedt/javafxdemo/SceneSwitcher.java
+++ src/main/java/be/bacdesmedt/javafxdemo/SceneSwitcher.java
@@ -1,9 +1,17 @@
 package be.bacdesmedt.javafxdemo;
 
 import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.layout.StackPane;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 public class SceneSwitcher extends Application {
+
+    Stage window;
+    Scene scene1, scene2;
 
     public static void main(String[] args) {
         launch(args);
@@ -11,6 +19,29 @@ public class SceneSwitcher extends Application {
 
     @Override
     public void start(Stage stage) throws Exception {
+        window = stage;
+        window.setTitle("Scene Switcher");
 
+        //SCENE I
+        Label label1 = new Label("Welcome to the first scene");
+        Button button1 = new Button("Go to scene 2");
+        VBox layout1 = new VBox(20);
+        scene1 = new Scene(layout1, 200, 200);
+
+        button1.setOnAction(e -> window.setScene(scene2));
+        layout1.getChildren().addAll(label1, button1);
+
+
+        //SCENE II
+        Button button2 = new Button("This scene sucks, go back to scene 1");
+        StackPane layout2 = new StackPane();
+        scene2 = new Scene(layout2, 400, 500);
+
+        button2.setOnAction(e -> window.setScene(scene1));
+        layout2.getChildren().add(button2);
+
+
+        window.setScene(scene1);
+        window.show();
     }
 }
